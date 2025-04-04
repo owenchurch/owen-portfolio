9be185abf6b55c9329e52b5141ef415a699dba15
Owen Church <owenchurch@gmail.com>
2025-04-04 18:16:01 -0400

    stuff

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <header className="">owen church</header>
-      <div className="btn">button</div>
+      <header className="p-6 text-center">
+        <h1 className="text-5xl font-bold p-4">owen church</h1>
+        <p className="text-xl opacity-90">web developer</p>
+      </header>
     </div>
   );
 }
