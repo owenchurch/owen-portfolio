565134d5dbf19bb953e5adcdc6810018d64cdc25
Owen Church <owenchurch@gmail.com>
2025-04-05 19:25:07 -0400

    fun gradient text

--- app/page.js
+++ app/page.js
@@ -2,8 +2,14 @@ export default function Home() {
   return (
     <div>
       <header className="p-6 text-center">
-        <h1 className="text-5xl font-bold p-4">owen church</h1>
-        <p className="text-xl opacity-90">web developer</p>
+        <h1 className=" font-bold p-4">
+          <span className="text-4xl opacity-80">hey my name is</span>
+          <br />
+          <span className="text-6xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500">
+            owen church
+          </span>
+        </h1>
+        <p className="text-xl opacity-80">im a web developer 👍</p>
       </header>
     </div>
   );
